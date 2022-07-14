265d1059b3d85cef69103d53475c7fce5771e492
Rednata <maknatosh@gmail.com>
2022-07-14 09:03:01 +0700

    complete task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let arr = [];
+    for (let i =0; i<expr.length; i+=10) {
+        if (expr.slice(i, i+10) === '**********') {
+            arr.push(' ');
+        }
+        else {
+            arr.push(expr.slice(i, i+10));
+        }
+    }
+   
+    arr.forEach((elem, ind, mass) => {
+        mass[ind] = elem.slice(elem.indexOf('1'));
+    });
+
+    const dotDash = (str) => {
+        let result = '';        
+        if (str === ' ') {
+            return ' ';
+        }
+        for (let i=0; i<str.length; i += 2) {
+            if (str.slice(i, i+2) === '10') {
+                result += '.' ;
+                
+            } else {
+                result += '-' ;
+            }         
+        }
+        return result;
+    }
+    let frase= ''
+    arr.forEach(elem => {
+        frase += MORSE_TABLE[dotDash(elem)] || ' ';
+    });
+      return frase;   
 }
 
 module.exports = {
     decode
 }
+
+
+
